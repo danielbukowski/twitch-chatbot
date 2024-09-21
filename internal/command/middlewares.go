6b5720ebda3d6a6a3e134926ac8a29648347c4cd
danielbukowski <daniel.bukowski01@gmail.com>
2024-09-21 23:57:55 +0200

    Add a middleware to handle errors

---
+++ internal/command/middlewares.go
@@ -0,0 +1,26 @@
+package command
+
+import (
+	"context"
+	"errors"
+
+	"go.uber.org/zap"
+)
+
+func ErrorHandler(logger *zap.Logger) Middleware {
+	return func(cb CallbackSignature) CallbackSignature {
+		return func(ctx context.Context, args []string, chatClient chatClient) error {
+
+			err := cb(ctx, args, chatClient)
+
+			if err != nil {
+				if errors.Is(err, errNoPermissions) {
+					return nil
+				}
+
+				logger.Error("An unhandled error occurred", zap.Error(err))
+			}
+			return nil
+		}
+	}
+}
